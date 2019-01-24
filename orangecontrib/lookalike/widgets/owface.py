648f42547193c36f547561df9f36f8801d4d0d23
Sven Bingert <sbinger1@Grimhilde.local>
2019-01-24 11:55:32 +0100

    fixed file path

--- orangecontrib/lookalike/widgets/owface.py
+++ orangecontrib/lookalike/widgets/owface.py
@@ -7,6 +7,7 @@ import urllib
 import cv2
 import numpy as np
 import sys
+import threading
 
 from Orange.data import Table, Domain, StringVariable
 from Orange.widgets import widget
@@ -77,6 +78,7 @@ class OWFace(widget.OWWidget):
 
     def find_face(self, file_path, face_path):
         """Find the face in image file_path and store it in face_path."""
+        #print(threading.currentThread().getName(), 'Starting')
         img = self.read_img(file_path)
         if img is None:
             return False
@@ -89,6 +91,7 @@ class OWFace(widget.OWWidget):
         x, y, w, h = max(faces, key=lambda xywh: xywh[2] * xywh[3])
         face = img[y:y+h, x:x+w]
         cv2.imwrite(face_path, face)
+        #print(threading.currentThread().getName(), 'Exiting')
         return True
 
     @staticmethod
@@ -112,6 +115,9 @@ class OWFace(widget.OWWidget):
             with tempfile.NamedTemporaryFile(suffix=file_ext, delete=False) as f:
                 face_abs = f.name
                 tmp_files.append(face_abs)
+            #t = threading.Thread(target=self.find_face, args=(file_abs, face_abs))
+            #threads.append(t)
+            #t.start()   
             if self.find_face(file_abs, face_abs):
                 faces_list.append([face_abs])
                 n_faces += 1
